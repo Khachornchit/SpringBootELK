f9913978d2e3f260f4a8bb4f54e9b20ea6c50a44
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-11-17 18:22:55 +0700

    Update Gradle 6.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot ELK
+
+Spring Boot Microservices and ELK(Elasticsearch, Logstash, and Kibana)
---
+++ src/test/java/net/putfirstthingsfirst/AppTests.java
@@ -0,0 +1,13 @@
+package net.putfirstthingsfirst;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
